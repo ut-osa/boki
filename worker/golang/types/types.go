5300e192d110b1e45a715dd112dab5f6d36116eb
Zhipeng Jia <zjia@cs.utexas.edu>
2020-12-10 01:55:29 -0600

    implement InvokeFuncAsync

--- worker/golang/types/types.go
+++ worker/golang/types/types.go
@@ -6,6 +6,7 @@ import (
 
 type Environment interface {
 	InvokeFunc(ctx context.Context, funcName string, input []byte) ( /* output */ []byte, error)
+	InvokeFuncAsync(ctx context.Context, funcName string, input []byte) error
 	GrpcCall(ctx context.Context, service string, method string, request []byte) ( /* reply */ []byte, error)
 }
 
