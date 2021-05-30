89502968eda86bb53db6d20eea55944c49c97460
Zhipeng Jia <zjia@cs.utexas.edu>
2021-05-29 23:04:16 -0500

    Dump jemalloc profile when memory usage is high

--- src/utils/jemalloc.cpp
+++ src/utils/jemalloc.cpp
@@ -4,11 +4,14 @@
 
 #define CTL_GET(n, v, t) do {              \
     size_t sz = sizeof(t);                 \
-    mallctl(n, (void *)v, &sz, NULL, 0);  \
+    mallctl(n, (void*)v, &sz, nullptr, 0); \
 } while (0)
 
 namespace faas {
 namespace jemalloc {
+
+static constexpr size_t kAllocatedThresholdForReport   = 1024;           // 1KB
+static constexpr size_t kAllocatedThresholdForProfDump = 1024*1024*1024; // 1GB
 
 namespace {
 bool UpdateEpoch() {
@@ -24,6 +27,10 @@ bool UpdateEpoch() {
     return true;
 }
 
+void ProfileDump(const char* file_path) {
+    mallctl("prof.dump", nullptr, nullptr, (void*)&file_path, sizeof(const char*));
+}
+
 std::string FormatBytes(size_t bytes) {
     if (bytes < size_t{10} * 1024) {
         return fmt::format("{}B", bytes);
@@ -35,6 +42,17 @@ std::string FormatBytes(size_t bytes) {
         return fmt::format("{:.1f}MB", static_cast<float>(bytes)/1024/1024);
     }
     return fmt::format("{:.1f}GB", static_cast<float>(bytes)/1024/1024/1024);
+}
+
+void WriteCallback(void* ptr, const char* s) {
+    std::stringstream* stream = reinterpret_cast<std::stringstream*>(ptr);
+    (*stream) << s;
+}
+
+std::string GetMallocStats(const char* opts) {
+    std::stringstream stream;
+    malloc_stats_print(&WriteCallback, &stream, opts);
+    return stream.str();
 }
 
 std::atomic<size_t> prev_allocated{0};
@@ -49,14 +67,20 @@ void PrintStat() {
     size_t allocated;
     CTL_GET("stats.allocated", &allocated, size_t);
 
-    static constexpr int64_t kAllocatedThreshold = 1024;
-    int64_t old_allocated = gsl::narrow_cast<int64_t>(
-        prev_allocated.load(std::memory_order_relaxed));
-    if (std::abs(old_allocated - gsl::narrow_cast<int64_t>(allocated)) < kAllocatedThreshold) {
+    size_t old_allocated = prev_allocated.load(std::memory_order_relaxed);
+    size_t delta = gsl::narrow_cast<size_t>(std::abs(
+        gsl::narrow_cast<int64_t>(old_allocated) - gsl::narrow_cast<int64_t>(allocated)));
+    if (delta < kAllocatedThresholdForReport) {
         // The difference is too tiny, will not print stats
         return;
     }
     prev_allocated.store(allocated);
+
+    if (old_allocated == 0) {
+        // First call of PrintStat, will invoke malloc_stats_print first
+        LOG(INFO) << "jemalloc general information:\n"
+                  << GetMallocStats("mdablxe");
+    }
 
     size_t resident;
     size_t mapped;
@@ -73,6 +97,14 @@ void PrintStat() {
               << "mapped="    << FormatBytes(mapped)    << ", "
               << "metadata="  << FormatBytes(metadata)  << ", "
               << "retained="  << FormatBytes(retained);
+
+    if (allocated >= kAllocatedThresholdForProfDump) {
+        bool prof_enabled;
+        CTL_GET("opt.prof", &prof_enabled, bool);
+        if (prof_enabled) {
+            ProfileDump(nullptr);
+        }
+    }
 }
 
 }  // namespace jemalloc
