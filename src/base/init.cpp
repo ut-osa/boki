24f3e0f42b6091cce369a91b28c6fc52f3e48a1b
Zhipeng Jia <zjia@cs.utexas.edu>
2022-01-31 16:12:44 -0600

    Invoke cleanup functions in reverse order

--- src/base/init.cpp
+++ src/base/init.cpp
@@ -49,9 +49,11 @@ static void RaiseToDefaultHandler(int signo) {
 static void SignalHandler(int signo) {
     if (signo == SIGTERM || signo == SIGABRT) {
         size_t n = next_cleanup_fn.load();
-        fprintf(stderr, "Invoke %d clean-up functions\n", (int) n);
-        for (size_t i = 0; i < n; i++) {
-            cleanup_fns[i]();
+        if (n > 0) {
+            fprintf(stderr, "Invoke %d clean-up functions\n", (int) n);
+            for (size_t i = n - 1; i >= 0; i--) {
+                cleanup_fns[i]();
+            }
         }
         fprintf(stderr, "Exit with failure\n");
         exit(EXIT_FAILURE);
